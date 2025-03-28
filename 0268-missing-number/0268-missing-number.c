a5ded9a3bb2df5661adbe5cac90726f534f76e1f
madhusree-1 <madhusreenallamsetti@gmail.com>
2025-03-28 07:09:12 +0530

    Time: 8 ms (14.33%), Space: 9.2 MB (9.29%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.c
@@ -0,0 +1,14 @@
+int compare(const void *a,const void *b){
+    return *(int*)a-*(int*)b;
+}
+int missingNumber(int* nums, int numsSize) {
+    qsort(nums,numsSize,sizeof(int),compare);
+    int k=0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=k) {
+            break;
+        }
+        k++;
+    }
+     return k;
+}
